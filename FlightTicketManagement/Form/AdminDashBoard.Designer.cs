ba49a935aa0e5f3b787f9ee450066f7481ff60d5
dovandat3915@gmail.com <dovandat3915@gmail.com>
2024-06-12 10:31:53 +0700

    Admin Dasboard

--- FlightTicketManagement/Form/AdminDashBoard.Designer.cs
+++ FlightTicketManagement/Form/AdminDashBoard.Designer.cs
@@ -28,12 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SidePanel = new System.Windows.Forms.Panel();
+            this.HeaderPanel = new System.Windows.Forms.Panel();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.AirportBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SidePanel.SuspendLayout();
+            this.HeaderPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SidePanel
+            // 
+            this.SidePanel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.SidePanel.Controls.Add(this.AirportBtn);
+            this.SidePanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.SidePanel.Location = new System.Drawing.Point(0, 51);
+            this.SidePanel.Name = "SidePanel";
+            this.SidePanel.Size = new System.Drawing.Size(132, 399);
+            this.SidePanel.TabIndex = 0;
+            // 
+            // HeaderPanel
+            // 
+            this.HeaderPanel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.HeaderPanel.Controls.Add(this.label1);
+            this.HeaderPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.HeaderPanel.Location = new System.Drawing.Point(0, 0);
+            this.HeaderPanel.Name = "HeaderPanel";
+            this.HeaderPanel.Size = new System.Drawing.Size(861, 51);
+            this.HeaderPanel.TabIndex = 1;
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.BackColor = System.Drawing.SystemColors.Info;
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(132, 51);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(729, 399);
+            this.MainPanel.TabIndex = 2;
+            // 
+            // AirportBtn
+            // 
+            this.AirportBtn.Location = new System.Drawing.Point(3, 224);
+            this.AirportBtn.Name = "AirportBtn";
+            this.AirportBtn.Size = new System.Drawing.Size(130, 45);
+            this.AirportBtn.TabIndex = 0;
+            this.AirportBtn.Text = "Sân bay";
+            this.AirportBtn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Admin";
+            // 
+            // AdminDashBoard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(861, 450);
+            this.Controls.Add(this.MainPanel);
+            this.Controls.Add(this.SidePanel);
+            this.Controls.Add(this.HeaderPanel);
+            this.Name = "AdminDashBoard";
             this.Text = "Form1";
+            this.SidePanel.ResumeLayout(false);
+            this.HeaderPanel.ResumeLayout(false);
+            this.HeaderPanel.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel SidePanel;
+        private System.Windows.Forms.Panel HeaderPanel;
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Button AirportBtn;
+        private System.Windows.Forms.Label label1;
     }
 }
--- FlightTicketManagement/Form/AdminDashBoard.cs
+++ FlightTicketManagement/Form/AdminDashBoard.cs
@@ -16,5 +16,10 @@ namespace FlightTicketManagement
         {
             InitializeComponent();
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
